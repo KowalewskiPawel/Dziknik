96ddf39fe361ed054acfa2c69166aee82cba126b
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-10-21 15:21:18 +0200

    feat: add database config

---
+++ backend/src/database/index.ts
@@ -0,0 +1,3 @@
+import { pool } from "./database";
+
+export { pool };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
+import { pool } from './database';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express and TypeScript!');
+app.get('/', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
