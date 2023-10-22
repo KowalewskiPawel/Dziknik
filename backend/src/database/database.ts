7a7d8ba93938daa4322ca0f45155e8e09d4d7799
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-10-22 17:46:12 +0200

    fix: improve code quality

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -7,5 +7,5 @@ export const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
-    port: Number(process.env.PORT),
+    port: Number(process.env.DB_PORT),
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,28 +1,29 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import swaggerUi from 'swagger-ui-express';
+import swaggerUi from "swagger-ui-express";
+import dotenv from "dotenv";
 import { pool } from "./database";
 import { bodySanitizer } from "./middlewares";
 import { usersRouter } from "./routes";
 import { swaggerDocs } from "./swagger";
 
+dotenv.config();
+
+const PORT = process.env.APP_PORT || 3000;
+
 const app = express();
-const PORT = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(cors())
+  .use(helmet())
+  .use(bodySanitizer)
+  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+  .use("/users", usersRouter);
 
-app.use(cors());
-app.use(helmet());
-
-app.use(bodySanitizer);
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-app.use("/users", usersRouter);
-
-app.get("/", async (req, res) => {
+app.get("/", async (_req, res) => {
   try {
     const result = await pool.query("SELECT NOW()");
     res.json(result.rows[0]);
@@ -31,6 +32,7 @@ app.get("/", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
